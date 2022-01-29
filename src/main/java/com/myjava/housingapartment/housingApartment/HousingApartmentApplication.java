19b7fc038d4b868d0223b1b716e214b108ede1e8
Tommi <tommi.korteikko@iCloud.com>
2022-01-29 09:06:25 +0200

    Initila commit for apartment

---
+++ src/main/java/com/myjava/housingapartment/housingApartment/HousingApartmentApplication.java
@@ -0,0 +1,13 @@
+package com.myjava.housingapartment.housingApartment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingApartmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousingApartmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myjava/housingapartment/housingApartment/HousingApartmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myjava.housingapartment.housingApartment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousingApartmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
